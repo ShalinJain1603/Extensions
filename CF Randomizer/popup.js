25d63c44c08421c04aa3e168ee51425f4514d11f
ShalinJain1603 <shalin@iitg.ac.in>
2021-09-04 19:28:45 +0530

    Added Range Filter

--- CF Randomizer/popup.js
+++ CF Randomizer/popup.js
@@ -1,55 +1,31 @@
-const default_Array = ["A", "B", "C", "D", "E"];
-const tracker = {
-  A: false,
-  B: false,
-  C: false,
-  D: false,
-  E: false,
-};
-const button_A = document.querySelector("#A");
-const button_B = document.querySelector("#B");
-const button_C = document.querySelector("#C");
-const button_D = document.querySelector("#D");
-const button_E = document.querySelector("#E");
-
-button_A.addEventListener("click", () => {
-  tracker["A"] = !tracker["A"];
-  button_A.classList.toggle("selected");
-});
-button_B.addEventListener("click", () => {
-  tracker["B"] = !tracker["B"];
-  button_B.classList.toggle("selected");
-});
-button_C.addEventListener("click", () => {
-  tracker["C"] = !tracker["C"];
-  button_C.classList.toggle("selected");
-});
-button_D.addEventListener("click", () => {
-  tracker["D"] = !tracker["D"];
-  button_D.classList.toggle("selected");
-});
-button_E.addEventListener("click", () => {
-  tracker["E"] = !tracker["E"];
-  button_E.classList.toggle("selected");
-});
-
 const getLink = document.querySelector("#getLink");
 const foundLink = document.querySelector("#link");
-getLink.addEventListener("click", () => {
-  const randomNum = parseInt(Math.random() * 1200) + 300;
+const minRating = document.querySelector("#minRating");
+const maxRating = document.querySelector("#maxRating");
+const radioButtons = document.querySelectorAll('input[name="index"]');
 
-  const selectionArray = [];
-  for (key in tracker) {
-    if (tracker[key]) {
-      selectionArray.push(key);
+getLink.addEventListener("click", () => {
+  let selectedIndex = "All";
+  for (button of radioButtons) {
+    if (button.checked) {
+      selectedIndex = button.id;
+      break;
     }
   }
-  const question =
-    selectionArray.length === 0
-      ? default_Array[parseInt(Math.random() * 4)]
-      : selectionArray[parseInt(Math.random() * selectionArray.length)];
-
-  const link = `https://codeforces.com/problemset/problem/${randomNum}/${question}`;
+  const lowerBar = parseInt(minRating.value) || 0;
+  const upperBar = parseInt(maxRating.value) || 3600;
+  let myQuestions = QUESTIONS.filter(
+    (question) => question.rating >= lowerBar && question.rating <= upperBar
+  );
+  if (selectedIndex !== "All") {
+    myQuestions = myQuestions.filter(
+      (question) => question.index === selectedIndex
+    );
+  }
+  const randomNum = parseInt(Math.random() * myQuestions.length);
+  const contestId = myQuestions[randomNum].contestId;
+  const index = myQuestions[randomNum].index;
+  const link = `https://codeforces.com/problemset/problem/${contestId}/${index}`;
   foundLink.innerText = link;
   foundLink.href = link;
 });
